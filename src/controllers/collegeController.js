87e195e303531e25ef2f81b3c71bdbdcf7a7a915
Ibrahim6231 <ibrahimkhan938@gmail.com>
2022-06-29 23:03:30 +0530

    commiting for pull

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -19,7 +19,7 @@ const createCollege = async function(req, res){
 const collegeInterns = async function(req, res){
     try {
         const collegeAbbreviation = req.query.collegeName;
-        if(!collegeAbbreviation) return res.status(400).send({status:false, message: "enter the abbreviated name of a college"}); //validation1
+        if(!collegeAbbreviation) return res.status(400).send({status:false, message: "enter the abbreviated name of a college in a collegeName key"}); //validation1
     
         let college = await collegeModel.findOne({name: collegeAbbreviation}).select({name:1, fullName:1, logoLink:1});
         if(!college) return res.status(404).send({status:false, message:"no college found"});   //validation2
@@ -28,6 +28,7 @@ const collegeInterns = async function(req, res){
         const internsList = await internModel.find({collegeId : clgId}).select({name:1, email:1, mobile:1});
         const result =  {...college.toJSON(), interns : internsList};
         delete result._id;
+        if(internsList.length === 0) result.interns = "currently, there a no any interns at this college"
 
         return res.status(200).send({status:true, data: result});
 
